e0a245c9cc65d4f79f60be4b1af199e952e3bb72
NakaRO96 <nakaionut@gmail.com>
2025-08-16 23:43:00 +0300

    eroare

--- models/User.js
+++ models/User.js
@@ -15,23 +15,20 @@ const userSchema = new mongoose.Schema({
       present: { type: Boolean, default: true }
     }
   ],
-  // Array-ul 'simulationResults' a fost ELIMINAT conform discuției anterioare,
-  // deoarece simulările sunt gestionate printr-o colecție separată ('SimulationResult').
 }, { timestamps: true });
 
-// ACEASTA ESTE SECȚIUNEA CRUCIALĂ: Hook-ul pre('save')
-// El va cripta parola DOAR dacă aceasta a fost modificată (ex: la crearea user-ului
-// sau la schimbarea parolei manuală)
+// Hook-ul pre('save') care criptează parola DOAR dacă a fost modificată
 userSchema.pre('save', async function(next) {
-  // Verifică dacă parola a fost modificată sau dacă este un document nou
   if (!this.isModified('password')) {
-    return next(); // Dacă parola nu s-a schimbat, mergi mai departe fără să o recriptezi
+    return next();
   }
-
-  // Criptează parola
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
   next();
 });
 
-module.exports = mongoose.model('User', userSchema);
+// === MODIFICARE CRUCIALĂ ȘI DEFINITIVĂ PENTRU A PREVENI OverwriteModelError ===
+// Aceasta verifică dacă modelul 'User' a fost deja definit.
+// Dacă `mongoose.models.User` există (adică modelul e deja înregistrat), îl refolosește.
+// Altfel, îl definește. Acest lucru previne eroarea de a defini același model de două ori.
+module.exports = mongoose.models.User || mongoose.model('User', userSchema);
