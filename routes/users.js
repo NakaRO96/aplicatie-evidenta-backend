d9048a877736859b4a3560e03677cdb451912120
NakaRO96 <nakaionut@gmail.com>
2025-08-21 23:49:33 +0300

    incercare eroare

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,14 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const express = require('express');
+const router = express.Router();
+const authMiddleware = require('../middleware/authMiddleware');
+const adminMiddleware = require('../middleware/adminMiddleware');
+const userController = require('../controllers/userController');
 
 // Ruta pentru a obține toți utilizatorii - necesită autentificare și rol de admin
 router.get('/', authMiddleware, adminMiddleware, userController.getAllUsers);
 
-// Hook-ul pre('save') care criptează parola DOAR dacă a fost modificată
-userSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) {
-    return next();
-  }
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
-});
+// Poți adăuga aici și alte rute pentru utilizatori
+// router.get('/:id', ...);
+// router.delete('/:id', ...);
 
-// === LINIA ESENȚIALĂ: Asigură că modelul este definit o singură dată și exportat corect ===
-// Aceasta verifică dacă modelul 'User' a fost deja definit. Dacă da, îl refolosește.
-// Altfel, îl definește. Acest lucru previne eroarea de a defini același model de două ori
-// și asigură că 'User' este întotdeauna un obiect Mongoose valid.
-module.exports = mongoose.models.User || mongoose.model('User', userSchema);
+module.exports = router;
